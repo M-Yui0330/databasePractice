9ac002507959b5b27c98a8be5271777a15de611b
M-Yui0330 <kuwamats.yui@gmail.com>
2025-03-26 10:01:34 +0900

    修正依頼の対応

--- pg_practice.sql
+++ pg_practice.sql
@@ -1,11 +1,15 @@
 -- 問題1. postgresqlでpracticeデータベースを生成するSQL文を記述してください。
 CREATE DATABASE
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
+
+-- 修正依頼の変更部分
+CREATE TYPE gender_type AS ENUM ('Man', 'Woman', 'Other');
+
 CREATE TABLE users (
 id SERIAL PRIMARY KEY,
 name VARCHAR(255) NOT NULL DEFAULT '',
 age INTEGER NOT NULL DEFAULT 0,
-gender VARCHAR(10) NOT NULL DEFAULT 'Other'
+gender gender_type NOT NULL DEFAULT 'Other'
 );
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
